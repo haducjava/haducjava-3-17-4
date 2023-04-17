bd8b33104455a544ed7cfce4d2976329ff574af4
unknown <haducgame@gmail.com>
2023-04-17 19:24:45 +0700

    bai 03

---
+++ Bai1KieuDuLieu/src/KieuDuLieu.java
@@ -0,0 +1,33 @@
+public class KieuDuLieu {
+    public static void main(String[] args) {
+        // số nguyên
+        byte x = 10;
+        short s = 30;
+        int i = 3000;
+        long l = 5000L;
+        System.out.println(x);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+
+        //số thực
+        float f = 10.100f;
+        double d = 20.200;
+        System.out.println(f);
+        System.out.println(d);
+
+        //kiểu đúng, sai
+        boolean t = true;
+        boolean b = false;
+        System.out.println(t);
+        System.out.println(b);
+        // kiểu ký tự
+        char a = 'A';
+        char c = 'b';
+        System.out.println(a);
+        System.out.println(c);
+
+
+
+    }
+}
